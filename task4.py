9fc2eda96b2db828c5a57091c30a8be9c6c0dd3e
daxsmith2025 <daxton.smith795@students.cmcss.net>
2026-03-06 13:10:17 -0600

    chicken sandwich 2.0

---
+++ task4.py
@@ -0,0 +1,37 @@
+good = r"""                          _,-'^\
+                       _,-'   ,\ )
+                   ,,-'     ,'  d'
+    ,,,           J_ \    ,'
+   `\ /     __ ,-'  \ \ ,'
+   / /  _,-'  '      \ \
+  / |,-'             /  }
+ (                 ,'  /
+  '-,________         /
+             \       /
+              |      |
+             /       |
+            /        |
+           /  /~\   (\/)
+          {  /   \     }
+          | |     |   =|
+          / |      ~\  |
+          J \,       (_o
+           '"
+"""
+bad = r"""
+  ,--.
+  \  _\_
+  _\/_|_\____.'\
+-(___.--._____(
+     \   \
+      \   \
+       `--'
+jg"""
+print(bad)
+
+drawbridge_down = False
+if drawbridge_down:
+    outcome = 'Thunder: Bolt to freedom'
+else:
+    outcome = 'Doom: Have to wait for the right time'
+print(outcome)
